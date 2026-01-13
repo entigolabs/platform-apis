f4a2ae0d1d8400a748f78de9932c993c35723b04
Jevgeni Fenko <jevgeni.fenko@gmail.com>
2026-01-13 11:48:04 +0200

    kafka render tests

---
+++ compositions/kafka/test/mocks.sh
@@ -0,0 +1,21 @@
+mock_cluster_ready_status() {
+  yq eval '
+    select(.kind == "Cluster") |
+    .status.atProvider.bootstrapBrokersSaslIam = "test-broker-saas-iam"|
+    .status.conditions = [{"type": "Synced", "status": "True"}, {"type": "Ready", "status": "True"}]
+  ' -
+}
+
+mock_msk_observer_ready_status() {
+  yq eval '
+    select(.kind == "MSK") |
+    .status.conditions = [{"type": "Synced", "status": "True"}, {"type": "Ready", "status": "True"}]
+  ' -
+}
+
+mock_xtopic_ready_status() {
+  yq eval '
+    select(.kind == "XTopic") |
+    .status.conditions = [{"type": "Synced", "status": "True"}, {"type": "Ready", "status": "True"}]
+  ' -
+}
