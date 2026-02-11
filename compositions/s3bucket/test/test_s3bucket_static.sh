923b27d41b1e0d9261e90e162c1e11cc1954a705
taivox <taivox@gmail.com>
2026-02-11 16:55:32 +0200

    fix s3bucket tests

--- compositions/s3bucket/test/test_s3bucket_static.sh
+++ compositions/s3bucket/test/test_s3bucket_static.sh
@@ -4,23 +4,41 @@ load_mocks
 init_test "s3bucket" "/workspace/functions/storage"
 setup_resources --env --required
 
-# Remove sequence-creation step for testing
-TEMP_COMPOSITION="temp_composition.yaml"
-cp "$COMPOSITION" "$TEMP_COMPOSITION"
-yq -i 'del(.spec.pipeline[] | select(.step == "sequence-creation"))' "$TEMP_COMPOSITION"
+echo "TEST 1: rendering step 1 resources (Bucket, User, Role)"
+OUTPUT=$(run_render "$INPUT" "$COMPOSITION" "$FUNC_CONFIG")
+assert_counts "$OUTPUT" "Bucket" 1 "User" 1 "Role" 1
 
-echo "TEST 1: rendering resources"
-OUTPUT=$(run_render "$INPUT" "$TEMP_COMPOSITION" "$FUNC_CONFIG")
-assert_counts "$OUTPUT" "User" 1 \
-                         "Secret" 1 \
-                         "SecretVersion" 1 \
-                         "UserPolicyAttachment" 1 \
-                         "RolePolicyAttachment" 1 \
-                         "Role" 1 \
-                         "Policy" 1 \
-                         "AccessKey" 1 \
-                         "BucketVersioning" 1 \
-                         "BucketServerSideEncryptionConfiguration" 1 \
-                         "BucketPublicAccessBlock" 1 \
-                         "BucketOwnershipControls" 1 \
-                         "Bucket" 1
+echo "Mocking observed resources"
+echo "$OUTPUT" | mock_observed_resources | start_observed
+
+echo "TEST 2: rendering step 2 resources (BucketVersioning, AccessKey, Policy)"
+OUTPUT=$(run_render "$INPUT" "$COMPOSITION" "$FUNC_CONFIG")
+assert_counts "$OUTPUT" "BucketVersioning" 1 "AccessKey" 1 "Policy" 1
+
+echo "Mocking observed resources"
+echo "$OUTPUT" | mock_observed_resources | start_observed
+
+echo "TEST 3: rendering step 3 resources (BucketOwnershipControls, UserPolicyAttachment, RolePolicyAttachment)"
+OUTPUT=$(run_render "$INPUT" "$COMPOSITION" "$FUNC_CONFIG")
+assert_counts "$OUTPUT" "BucketOwnershipControls" 1 "UserPolicyAttachment" 1 "RolePolicyAttachment" 1
+
+echo "Mocking observed resources"
+echo "$OUTPUT" | mock_observed_resources | start_observed
+
+echo "TEST 4: rendering step 4 resources (BucketPublicAccessBlock, Secret)"
+OUTPUT=$(run_render "$INPUT" "$COMPOSITION" "$FUNC_CONFIG")
+assert_counts "$OUTPUT" "BucketPublicAccessBlock" 1 "Secret" 1
+
+echo "Mocking observed resources"
+echo "$OUTPUT" | mock_observed_resources | start_observed
+
+echo "TEST 5: rendering step 5 resources (BucketServerSideEncryptionConfiguration, SecretVersion)"
+OUTPUT=$(run_render "$INPUT" "$COMPOSITION" "$FUNC_CONFIG")
+assert_counts "$OUTPUT" "BucketServerSideEncryptionConfiguration" 1 "SecretVersion" 1
+
+echo "Mocking observed resources"
+echo "$OUTPUT" | mock_observed_resources | start_observed
+
+echo "TEST 6: Checking S3Bucket Readiness"
+OUTPUT=$(run_render "$INPUT" "$COMPOSITION" "$FUNC_CONFIG")
+assert_ready "$OUTPUT" "S3Bucket"
