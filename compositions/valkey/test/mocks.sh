228e083a1f8c09d63210213f5d9b61582218f5db
taivox <taivox@gmail.com>
2026-02-14 16:22:02 +0200

    rewrite valkey

--- compositions/valkey/test/mocks.sh
+++ compositions/valkey/test/mocks.sh
@@ -5,11 +5,7 @@ mock_observed_resources() {
     (select(.kind == "ReplicationGroup") |
       .status.atProvider.primaryEndpointAddress = "example.cache.amazonaws.com" |
       .status.atProvider.port = 6379 |
-      .status.conditions = [{"type": "Synced", "status": "True"}, {"type": "Ready", "status": "True"}] |
-      .connectionDetails."attribute.auth_token" = "dGVzdC1hdXRoLXRva2Vu" |
-      .connectionDetails.port = "NjM3OQ==" |
-      .connectionDetails.primary_endpoint_address = "ZXhhbXBsZS5jYWNoZS5hbWF6b25hd3MuY29t" |
-      .connectionDetails.reader_endpoint_address = "ZXhhbXBsZS1yby5jYWNoZS5hbWF6b25hd3MuY29t"
+      .status.conditions = [{"type": "Synced", "status": "True"}, {"type": "Ready", "status": "True"}]
     ),
     (select(.kind == "SecurityGroup" or .kind == "SecurityGroupRule" or .kind == "Secret" or .kind == "SecretVersion") |
       .status.conditions = [{"type": "Synced", "status": "True"}, {"type": "Ready", "status": "True"}]
