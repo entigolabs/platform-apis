b89107b863b207de401c401447d5faaa3bde6eed
Märt Erlenheim <71629159+entigo-mart-erlenheim@users.noreply.github.com>
2026-02-16 15:59:41 +0200

    Added locale to database (#113)
    * fix helm versioning
    * infralib tests workflow fix
    * Added locale to database
    * add database locale, add postgresqluser external name, add postgresql tests
    
    Co-authored-by: martivo <mardicas@gmail.com>
    Co-authored-by: Jevgeni Fenko <jevgeni.fenko@gmail.com>

---
+++ test/tests/k8s_unit_basic_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/entigolabs/entigo-infralib-common/k8s"
+	terrak8s "github.com/gruntwork-io/terratest/modules/k8s"
+)
+
+const (
+	ConfigurationKind = "configuration.pkg.crossplane.io"
+)
+
+func TestK8sPlatformApisAWSBiz(t *testing.T) {
+	testK8sPlatformApis(t, "aws", "biz")
+}
+
+// func TestK8sPlatformApisAWSPri(t *testing.T) {
+// 	testK8sPlatformApis(t, "aws", "pri")
+// }
+
+func testK8sPlatformApis(t *testing.T, cloudName string, envName string) {
+	t.Parallel()
+	kubectlOptions, _ := k8s.CheckKubectlConnection(t, cloudName, envName)
+
+	argocdNamespace := fmt.Sprintf("argocd-%s", envName)
+	argocdOptions := terrak8s.NewKubectlOptions(kubectlOptions.ContextName, kubectlOptions.ConfigPath, argocdNamespace)
+
+	clusterOptions := terrak8s.NewKubectlOptions(kubectlOptions.ContextName, kubectlOptions.ConfigPath, "")
+
+	testPlatformApisZone(t, argocdNamespace, clusterOptions, argocdOptions)
+	testPlatformApisPostgresql(t, argocdNamespace, clusterOptions)
+}
