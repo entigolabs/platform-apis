f2af5d9d9dd098ff5fb3f28c473db1be5871372d
Jevgeni Fenko <jevgeni.fenko@gmail.com>
2026-02-16 20:19:49 +0200

    remove usage

--- compositions/postgresql/test/test_database_static.sh
+++ compositions/postgresql/test/test_database_static.sh
@@ -3,6 +3,6 @@
 load_mocks
 init_test "database"
 
-echo "TEST 1: rendering Grant, Database and Usage..."
+echo "TEST 1: rendering Grant and Database..."
 OUTPUT=$(run_render "$INPUT" "$COMPOSITION" "$FUNC_CONFIG")
-assert_counts "$OUTPUT" "Grant" 1 "Database" 1 "Usage" 1
+assert_counts "$OUTPUT" "Grant" 1 "Database" 1
