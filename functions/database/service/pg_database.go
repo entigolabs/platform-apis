aac7317a9776f7232ec08189ec06226bdc49ebf3
Jevgeni Fenko <jevgeni.fenko@gmail.com>
2026-03-18 15:56:29 +0200

    force pipeline run

--- functions/database/service/pg_database.go
+++ functions/database/service/pg_database.go
@@ -18,7 +18,6 @@ import (
 )
 
 const CrossplaneProtectionApi = "protection.crossplane.io/v1beta1"
-const dummyTestConst = "dummy"
 
 type pgDatabaseGenerator struct {
 	pgDatabase         v1alpha1.PostgreSQLDatabase
